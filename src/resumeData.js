0a94d9e6f1a9f5e2be980c17750747389a24f4bb
Sahil Patel <sahildepatel@gmail.com>
2022-07-24 19:38:06 -0400

    Seeing if works

---
+++ src/resumeData.js
@@ -0,0 +1,69 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/RubenCastroCoding",
+    "name": "Ruben Castro",
+    "role": "Fullstack Developer",
+    "roleDescription": " I work as a Regional sales manager  .",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/ruben-castro-66a234230/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/RubenCastroCoding",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"Hello, I'm Ruben Castro, and thank you for visiting my portfolio page. I'm currently a student of the Georgia Tech Coding Boot Camp, I am also a sales manager at Gee Hvac with over 12 years of experience in sales and business management. In addition to working, soccer and coding are some of my favorite hobbies.",
+    "education":[
+      {
+        "UniversityName":"Lanier Technical College",
+        "MonthOfPassing":"August",
+        "YearOfPassing":"2020",
+        "Achievements":"Associates of Industrial Systems Technology"
+      },
+      
+    ],
+    "work":[
+      {
+        "CompanyName":"GEE HVAC",
+        "specialization":"Regional Sales Manager",
+        "MonthOfLeaving":"Sep",
+        "YearOfLeaving":"2015",
+      },
+    ],
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"My Trash Your Treasure",
+        "description":"mobileapp",
+        "imgurl":"images/project1.png"
+      },
+      {
+        "name":"Resturant App",
+        "description":"mobileapp",
+        "imgurl":"images/portfolioProj2.jpg"
+        
+      },
+      //{
+      //   "name":"project3",
+      //   "description":"mobileapp",  
+      //   "imgurl":"images/portfolio/project2.png" 
+      // },
+      // {
+      //   "name":"project4",
+      //   "description":"mobileapp",
+      //   "imgurl":"images/portfolio/phone.jpg"
+      // }
+    ],
+  }
+  
+  export default resumeData
